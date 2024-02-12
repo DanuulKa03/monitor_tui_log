dceb600ee228459c14ccc49290fa064984c00abd
Renata <renata.talacheva@yandex.ru>
2024-02-12 21:33:59 +0300

    Checked the abiliti to read the file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include "Model/MainWindow/include/MainWindow.h"
+#include <string>
+#include <fstream>
+#include <iostream>
 
 
-int main() {
+void fileValidator (std::string string){
+    
+}
+
+int main(int argc, char* argv[]) {
     MainWindow app;
-    app.Run();
+  //  app.Run();
+    if (argc != 3) {
+        std::cerr << "Err" << std::endl;
+        return 1;
+    }
+
+    std::string filename = argv[2];
+    std::cout << "File: " << filename  << std::endl;
+    
+     std::string line;
+ 
+    std::ifstream in(filename); 
+    if (in.is_open())
+    {
+        while (std::getline(in, line))
+        {
+            std::cout << line << std::endl;
+        }
+    }
+    in.close();     
+
+
     return 0;
+
 }
 
