519b4623ae8ced6d78c7da7bedeb4eb4f6274fd0
Renata <renata.talacheva@yandex.ru>
2024-02-16 00:00:58 +0300

    Refactored code into functions, tokenized string, and introduced enumeration

--- main.cpp
+++ main.cpp
@@ -4,41 +4,88 @@
 #include <iostream>
 #include <regex>
 
-void fileValidator (std::string string){
+enum class Token {
+    DateTime,
+    Firmware,
+    Owner,
+    Payload
+};
+
+
+
+std::ifstream openFile(const std::string& filename) {
+    std::ifstream file(filename);
     
 }
 
-int main(int argc, char* argv[]) {
-    MainWindow app;
-  //  app.Run();
-    if (argc != 3) {
-        std::cerr << "Err" << std::endl;
-        return 1;
-    }
-
-    std::string filename = argv[2];
-    std::cout << "File: " << filename  << std::endl;
+void fileValidator (std::ifstream& in){
     
-     std::string line;
- 
-    std::ifstream in(filename); 
+    std::string line;
+
     if (in.is_open())
     {
 
-        std::smatch match;
-        std::regex pattern("\\[(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\\] \\(Firmware:(Info|Debug|Warning|Critical|Fatal)\\) \\b(\\w+): ");
+        std::smatch match_datetime;
+        std::smatch match_firmware;
+        std::smatch match_owner;
+
+        // паттерны полей полезной информации из строки
+        std::regex pat_datetime("\\[(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})\\] ");
+        std::regex pat_firmware("\\(Firmware:(Info|Debug|Warning|Critical|Fatal)\\) ");
+        std::regex pat_owner("\\b(\\w+): ");
 
         while (std::getline(in, line))
         {
-            if (std::regex_search(line, match, pattern)) {
-                std::cout << "Useful part: " << match.str()<<  match.suffix() << std::endl;
+            if (std::regex_search(line, match_datetime, pat_datetime)
+                && std::regex_search(line, match_firmware, pat_firmware)
+                && std::regex_search(line, match_owner, pat_owner)) {
+                
+                std::string match_payload = match_owner.suffix(); // собираем подстроку после последнего совпадения в осташейся строке
+               std::cout << "Useful part: " << match_datetime.str() 
+                         << match_firmware.str() << match_owner.str()
+                         << match_payload << std::endl;
+
+                Token token = Token::Firmware;
+                   /* if (std::regex_search(line, match_datetime, pat_datetime)) {
+                        token = Token::DateTime;
+                    } else if (std::regex_search(line, match_firmware, pat_firmware)) {
+                        token = Token::Firmware;
+                    } else if (std::regex_search(line, match_owner, pat_owner)) {
+                        token = Token::Owner;
+                    } else {
+                        token = Token::Payload;
+                }*/
+
+              //  std::cout << "Token: " << static_cast<int>(token) << std::endl;
+
             } else {
                 std::cout << "Useful part not found." << std::endl;
             }
         }
     }
-    in.close();     
-    
+    in.close();    
+}
+
+void processArguments(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Err" << std::endl;
+        return;
+    }
+
+    std::string filename = argv[2];
+    std::cout << "File: " << filename  << std::endl;
+    std::ifstream in(filename);
+
+    fileValidator(in); 
+     
+}
+
+
+
+int main(int argc, char* argv[]) {
+    MainWindow app;
+  //  app.Run();
+    processArguments(argc, argv); 
     return 0;
 
 }
