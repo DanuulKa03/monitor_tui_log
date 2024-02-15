ad943e2afe45bfffdefaab150687a188ab1183a7
Danuula_PC_Ubun <daniil.kazakov@piklema.com>
2024-02-15 18:59:23 +0300

    Сохраню на всякий случай

--- Model/MainWindow/src/MainWindow.cpp
+++ Model/MainWindow/src/MainWindow.cpp
@@ -38,7 +38,7 @@ void MainWindow::Run() {
                     });
     }) | bgcolor(Color::Red);
     auto buttonAdd = Button("Add", [&]{
-        container->Add(Button("Feb 05 18:36:49 imx8 PiklemaLauncher[83418]: [2024-02-05 18:36:49] (Firmware:Info) Controller: Message to enqueue: {\"course\":\"0\",\"gps_quality\":\"2.08\",\"gps_time\":\"183648\",\"hdop\":\"2.08\",\"height\":\"111.8\",\"lat\":\"51.310811\",\"lon\":\"40.504652\",\"satelite_count\":\"6\",\"speed\":\"0.859328\",\"time\":\"1707158209623\",\"x_acceleration\":\"0\",\"x_vibration_amplitude\":\"0\",\"x_vibration_frequency\":\"0\",\"x_vibration_speed\":\"0\",\"y_acceleration\":\"0\",\"y_vibration_amplitude\":\"0\",\"y_vibration_speed\":\"0\",\"z_acceleration\":\"0\",\"z_vibration_amplitude\":\"0\",\"z_vibration_frequency\":\"0\",\"z_vibration_speed\":\"0\"}, size: 460, schema_id: 70", [&]{}, ButtonOption::Ascii()));
+        container->Add(Button("Hello", [&]{}, ButtonOption::Ascii()) | flex );
         screen.Print();
     });
     //
