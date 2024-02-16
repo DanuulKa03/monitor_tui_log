de79d5063a3c50b06b835063035e534195414643
Danuula_PC_Ubun <daniil.kazakov@piklema.com>
2024-02-16 21:22:04 +0300

    Создал свой MyCollapsible. Пока рабоатет плохо

---
+++ Model/MyCollapsible/include/MyCollapsible.hpp
@@ -0,0 +1,23 @@
+//
+// Created by dika on 15.02.2024.
+//
+
+#ifndef LOG_MONITOR_MYCOLLAPSIBLE_HPP
+#define LOG_MONITOR_MYCOLLAPSIBLE_HPP
+
+#include <memory>      // for shared_ptr, allocator
+#include <utility>     // for move
+
+#include "ftxui/component/component.hpp"  // for Checkbox, Maybe, Make, Vertical, Collapsible
+#include "ftxui/component/component_base.hpp"  // for Component, ComponentBase
+#include "ftxui/component/component_options.hpp"  // for CheckboxOption, EntryState
+#include "ftxui/dom/elements.hpp"  // for operator|=, text, hbox, Element, bold, inverted
+#include "ftxui/util/ref.hpp"  // for Ref, ConstStringRef
+
+using namespace ftxui;
+
+Component MyCollapsible(ConstStringRef label,
+                      Component child,
+                      Ref<bool> show = false);
+
+#endif //LOG_MONITOR_MYCOLLAPSIBLE_HPP
---
+++ Model/MyCollapsible/src/MyCollapsible.cpp
@@ -0,0 +1,32 @@
+//
+// Created by dika on 15.02.2024.
+//
+
+#include"../include/MyCollapsible.hpp"
+
+Component MyCollapsible(ConstStringRef label, Component child, Ref<bool> show) {
+    class Impl : public ComponentBase {
+    public:
+        Impl(ConstStringRef label, Component child, Ref<bool> show) : show_(show) {
+            CheckboxOption opt;
+            opt.transform = [](EntryState s) {            // NOLINT
+                auto prefix = text(s.state ? "▼ " : "▶ ");  // NOLINT
+                auto t = paragraph(s.label);
+                if (s.active) {
+                    t |= bold;
+                }
+                if (s.focused) {
+                    t |= inverted;
+                }
+                return hbox({prefix, t});
+            };
+            Add(Container::Vertical({
+                                            Checkbox(label, show_.operator->(), opt),
+                                            Maybe(std::move(child), show_.operator->()),
+                                    }));
+        }
+        Ref<bool> show_;
+    };
+
+    return Make<Impl>(std::move(label), std::move(child), show);
+}
